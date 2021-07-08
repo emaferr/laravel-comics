4c865c72c379777df455de6f5f42f0f67c0029aa
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-07-08 17:21:12 +0200

    Import comics.php Set Route home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $comics = config('comics.data');
+    return view('home',compact('comics'));
+})->name('home');
