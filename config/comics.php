141d1e92b95f33c320fefe89e5511ee7268e8f68
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-07-10 00:33:45 +0200

    Add footer links to comics.php

--- config/comics.php
+++ config/comics.php
@@ -215,6 +215,69 @@ return [
             ]
         ]
 
+    ],
+
+    'footerLink' => [
+        [
+            'title' => 'DC COMICS',
+            'links' => ['Characters','Comics','Movies','TV','Games','Videos','News']
+        ],
+        [
+            'title' => 'DC',
+            'links' => ['Terms Of Use','Privacy policy (New)','Ad Choise','Advertising','jobs','Subscriptions','Talent Workshops','CPSC Certificates','Ratings','Shop Help','Contact Us']
+        ],
+        [
+            'title' => 'SITES',
+            'links' => ['DC','MAD Magazine','DC Kids','DC Universe','DC Power Visa']
+        ],
+        [
+            'title' => 'SHOP',
+            'links' => ['Shop DC','Shop DC Collectibles']
+        ],
+    ],
+
+    'navLink' => [
+        [
+            'title' => 'characters',
+            'href' => 'characters'
+        ],
+        [
+            'title' => 'comics',
+            'href' => 'comics'
+        ],
+        [
+            'title' => 'movies',
+            'href' => 'movies'
+        ],
+        [
+            'title' => 'tv',
+            'href' => 'tv'
+        ],
+        [
+            'title' => 'games',
+            'href' => 'games'
+        ],
+        [
+            'title' => 'collectibles',
+            'href' => 'collectibles'
+        ],
+        [
+            'title' => 'videos',
+            'href' => 'videos'
+        ],
+        [
+            'title' => 'fans',
+            'href' => 'fans'
+        ],
+        [
+            'title' => 'news',
+            'href' => 'news'
+        ],
+        [
+            'title' => 'shop',
+            'href' => 'shop'
+        ],
+        
     ]
      
 ];
