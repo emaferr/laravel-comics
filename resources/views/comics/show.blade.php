a03801e26b047c6da74d56ff400031cf46113d13
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-07-10 02:33:34 +0200

    Add layout for  comic

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,20 +5,14 @@
 @section('content')
 
 <main>
-    <h1>Fumetto singolo</h1>
-    <div class="comics_container py-5">
+    <div class="blue_line">
         <div class="container">
-            <div class="comics row">
-            <div class="col-2 py-3" style="height: max-content;">
-                <div class="crop">
-                    <img src="{{$comic['thumb']}}" class="card-img-top" alt="...">
-                </div>
-                <div id="card_body">
-                  <h6 class="card-text text-white">{{$comic['series']}}</h6>
-                </div>
-            </div>
-            </div>                
+            <div class="">
+                <img src="{{$comic['thumb']}}" class="" alt="...">
+            </div>              
+    
         </div>
+
     </div>
 </main>
 @endsection
